0945ce4af0946a79a892fd3c5c5dd8c1b160e7d0
cpf <cpfprogrammer@163.com>
2025-04-22 00:32:50 +0800

    文件扩展操作

--- 33 文件的扩展操作.py
+++ 33 文件的扩展操作.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+
 # 创建目录
 if not os.path.exists('new_dir'):
     os.mkdir('new_dir')
@@ -17,8 +18,8 @@ with open('需要删除的文件.txt', 'w') as f:
     pass
 os.remove('需要删除的文件.txt')
 # 复制文件
-with open('复制示例文件.txt', 'w', encoding='utf-8') as f:
-    f.write('120')
+with open('复制示例文件.txt', 'w', encoding='utf-8') as file:
+    file.write('120')
 shutil.copy('复制示例文件.txt', '复制后的文件.txt')
 # 移动文件
 if not os.path.exists('new_dir'):
@@ -37,3 +38,17 @@ except FileNotFoundError:
     print("文件未找到")
 except IOError:
     print('读写文件失败')
+# 文件指针与tell() seek()
+with open('文件的扩展操作.txt', 'w', encoding='utf-8') as f:
+    f.write('0123456789')
+with open('文件的扩展操作.txt', 'r', encoding='utf-8') as f:
+    print(f.tell())
+    f.seek(5)
+    print(f.tell())
+    print(f.read())
+# 读取图片
+with open('profile.jpg', 'rb') as f:
+    data = f.read()
+    print(data)
+with open('new_pic.jpg', 'wb') as f:
+    f.write(data)
